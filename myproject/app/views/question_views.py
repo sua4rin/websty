b286b13389fc5b1c26fb703da86a787626461dc6
sua4rin <166189954+sua4rin@users.noreply.github.com>
2024-05-20 20:15:13 +0900

    Add files via upload

---
+++ myproject/app/views/question_views.py
@@ -0,0 +1,97 @@
+import os
+from datetime import datetime
+from flask import Blueprint, render_template, request, url_for, g, flash, send_from_directory, abort
+from werkzeug.utils import redirect
+from werkzeug.utils import secure_filename
+from .. import db
+from ..forms import QuestionForm, AnswerForm
+from ..models import Question
+from ..views.auth_views import login_required
+
+
+
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+bp = Blueprint('question', __name__, url_prefix='/question')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/list/')
+def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
+    question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)
+    return render_template('question/question_list.html', question_list=question_list)
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    form = AnswerForm()
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question, form=form)
+
+
+@bp.route('/create/', methods=('GET', 'POST'))
+@login_required
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        file = request.files['file']
+        filename = None
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            file.save(file_path)
+
+        question = Question(subject=form.subject.data, content=form.content.data,
+                            create_date=datetime.now(), user=g.user, file=filename)
+
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html', form=form)
+
+
+@bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    if request.method == 'POST':
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now()  # 수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else:
+        form = QuestionForm(obj=question)
+    return render_template('question/question_form.html', form=form)
+
+@bp.route('/delete/<int:question_id>')
+@login_required
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
+
+@bp.route('/download/<filename>')
+def download_file(filename):
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+    if not os.path.exists(file_path):
+        flash('요청하신 파일이 존재하지 않습니다.', 'error')
+        return redirect(url_for('question._list'))
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+
+
